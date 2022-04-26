f722d2b934c2499424069b59ea0c79faa2da32fc
Tom Klein <tomjoklein@gmail.com>
2022-04-26 14:50:28 +0200

    refactor script

--- src/fine_tune.py
+++ src/fine_tune.py
@@ -4,40 +4,92 @@ import os
 import tensorflow as tf
 
 datadir = '/home/tom/Desktop/images/water'
-
 BATCH_SIZE = 32
 IMG_SIZE = (150, 200)
+k = 4
+num_epochs = 100
 
-train_dataset = tf.keras.utils.image_dataset_from_directory(datadir,
-                                                            shuffle=True,
-                                                            batch_size=BATCH_SIZE,
-                                                            image_size=IMG_SIZE)
-
-class_names = train_dataset.class_names
-
-# plt.figure(figsize=(10, 10))
-# for images, labels in train_dataset.take(1):
-#     for i in range(6):
-#         ax = plt.subplot(3, 3, i + 1)
-#         plt.imshow(images[i].numpy().astype("uint8"))
-#         plt.title(class_names[labels[i]])
-#         plt.axis("off")
-#     plt.show()
-
-AUTOTUNE = tf.data.AUTOTUNE
-train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
-
-data_augmentation = tf.keras.Sequential([
-    tf.keras.layers.RandomContrast(0.3),
-    tf.keras.layers.RandomBrightness(0.4),
-])
-
-for image, _ in train_dataset.take(5):
+def show_image_and_label(train_dataset):
     plt.figure(figsize=(10, 10))
-    first_image = image[0]
-    for i in range(6):
-        ax = plt.subplot(3, 3, i + 1)
-        augmented_image = data_augmentation(tf.expand_dims(first_image, 0))
-        plt.imshow(augmented_image[0] / 255)
-        plt.axis('off')
-    plt.show()
+    for images, labels in train_dataset.take(1):
+        for i in range(6):
+            plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.title(class_names[labels[i]])
+            plt.axis("off")
+        plt.show()
+
+
+def get_data_augmentation():
+    data_augmentation = tf.keras.Sequential([
+        tf.keras.layers.RandomContrast(0.3),
+        tf.keras.layers.RandomBrightness(0.4),
+    ])
+    return data_augmentation
+
+def show_altered_image(data_augmentation, train_dataset):
+    for image, _ in train_dataset.take(5):
+        plt.figure(figsize=(10, 10))
+        first_image = image[0]
+        for i in range(6):
+            plt.subplot(3, 3, i + 1)
+            augmented_image = data_augmentation(tf.expand_dims(first_image, 0))
+            plt.imshow(augmented_image[0] / 255)
+            plt.axis('off')
+
+        plt.show()
+
+
+def get_kfolded_datasets(train_data: list, train_targets: list, num_val_samples: int, i: int):
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
+    return val_data, val_targets, partial_train_data, partial_train_targets
+
+
+def convert_train_datasets(train_datasets):
+    train_data = []
+    train_targets = []
+    for image, label in train_datasets:
+        train_data.append(image)
+        train_targets.append(label)
+    return train_data, train_targets
+
+
+if __name__ == '__main__':
+    train_datasets = tf.keras.utils.image_dataset_from_directory(datadir,
+                                                                shuffle=True,
+                                                                batch_size=BATCH_SIZE,
+                                                                image_size=IMG_SIZE)
+    class_names = train_datasets.class_names
+
+    # add more training data
+    # data_augmentation = get_data_augmentation()
+
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_dataset = train_datasets.prefetch(buffer_size=AUTOTUNE)
+
+    preprocess_input = tf.keras.applications.mobilenet_v3.preprocess_input
+    IMG_SHAPE = IMG_SIZE + (3,)
+    base_model = tf.keras.applications.MobileNetV3Large(input_shape=IMG_SHAPE,
+                                                        include_top=False,
+                                                        weights='imagenet')
+    base_model.trainable = False
+    base_model.summary()
+    # train_data, train_targets = convert_train_datasets(train_datasets)
+    # num_val_samples = len(train_datasets) // k
+    #
+    # for i in range(k):
+    #     print(f"Processing fold #{i}")
+    #     val_data, val_targets, partial_train_data, partial_train_targets = get_kfolded_datasets(train_data, train_targets, num_val_samples)
+    #     # model compile
+    #
+    #     # model fit
+    #
