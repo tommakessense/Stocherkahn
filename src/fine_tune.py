ac550d99794d66dea692961c49ae65860e5f4ca3
Tom Klein <tomjoklein@gmail.com>
2022-04-29 15:11:11 +0200

    model compile and fit

--- src/fine_tune.py
+++ src/fine_tune.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import tensorflow as tf
 from tensorflow.python.ops.gen_dataset_ops import BatchDataset
+from tensorflow import keras
+from keras import layers
 
 datadir = '/home/tom/Desktop/images/water'
 BATCH_SIZE = 8
@@ -41,15 +43,42 @@ def create_additional_datasets(data_augmentation, train_datasets, size=8):
     dataset = tf.data.Dataset.from_tensor_slices((augmented_images, augmented_labels))
     return dataset
 
+def build_model_kfold(base_model, shape, data_augmentation):
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    prediction_layer = tf.keras.layers.Dense(1)
+    inputs = tf.keras.Input(shape=shape)
+    # x = data_augmentation(inputs)
+    x = tf.keras.applications.mobilenet_v3.preprocess_input(inputs)
+    x = base_model(x, training=False)
+    x = global_average_layer(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    outputs = prediction_layer(x)
+    kmodel = tf.keras.Model(inputs, outputs)
+    kmodel.compile(optimizer="rmsprop", loss="mse", metrics=["accuracy"])
+    return kmodel
 
-def get_kfolded_datasets(train_data: list, train_targets: list, num_val_samples: int, i: int):
+def get_kfolded_datasets(train_data: list, train_targets: list, num_val_samples: int, i: int, batch_size: int = 8):
+
+    def batch(dataset, size):
+        batch_set = []
+        batched = []
+        for img in dataset:
+            if len(batch_set) == size:
+                batched.append(batch_set)
+                batch_set = []
+            batch_set.append(img)
+        return batched
+
     val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
     val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
     partial_train_data = train_data[:i * num_val_samples]
     partial_train_data.extend(train_data[(i + 1) * num_val_samples:])
     partial_train_targets = train_targets[:i * num_val_samples]
     partial_train_targets.extend(train_targets[(i + 1) * num_val_samples:])
-    return val_data, val_targets, partial_train_data, partial_train_targets
+    return batch(val_data, batch_size), \
+           batch(val_targets, batch_size),\
+           batch(partial_train_data, batch_size), \
+           batch(partial_train_targets, batch_size)
 
 
 def convert_train_datasets(train_datasets):
@@ -74,22 +103,53 @@ if __name__ == '__main__':
     train_datasets = train_datasets.concatenate(additional_datasets)
     AUTOTUNE = tf.data.AUTOTUNE
     train_dataset = train_datasets.prefetch(buffer_size=AUTOTUNE)
+    train_dataset = train_dataset.batch(BATCH_SIZE)
+    train_data, train_targets = convert_train_datasets(train_datasets)
 
     preprocess_input = tf.keras.applications.mobilenet_v3.preprocess_input
     IMG_SHAPE = IMG_SIZE + (3,)
     base_model = tf.keras.applications.MobileNetV3Large(input_shape=IMG_SHAPE,
                                                         include_top=False,
                                                         weights='imagenet')
+    image_batch, label_batch = next(iter(train_dataset))
+    feature_batch = base_model(image_batch)
     base_model.trainable = False
     # base_model.summary()
-    train_data, train_targets = convert_train_datasets(train_datasets)
-    num_val_samples = len(train_datasets) // k
 
-    for i in range(k):
-        print(f"Processing fold #{i}")
-        val_data, val_targets, partial_train_data, partial_train_targets = get_kfolded_datasets(train_data, train_targets, num_val_samples, i)
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    prediction_layer = tf.keras.layers.Dense(1)
+
+    inputs = tf.keras.Input(shape=IMG_SHAPE)
+    # x = data_augmentation(inputs)
+    x = tf.keras.applications.mobilenet_v3.preprocess_input(inputs)
+    x = base_model(x, training=False)
+    x = global_average_layer(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    outputs = prediction_layer(x)
+    model = tf.keras.Model(inputs, outputs)
+
+    base_learning_rate = 0.0001
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+    initial_epochs = 10
+    split = len(train_dataset) // 4
+    validation_split = train_dataset.range(0, split)
+    train_split = train_dataset.range(split, len(train_dataset) - 1)
+    history = model.fit(train_split,
+                        epochs=initial_epochs,
+                        validation_data=validation_split)
+
+    # num_val_samples = len(train_datasets) // k
+    # all_mae_histories = []
+    # for i in range(k):
+    # print(f"Processing fold #{i}")
+    # val_data, val_targets, partial_train_data, partial_train_targets = get_kfolded_datasets(train_data, train_targets, num_val_samples, i, BATCH_SIZE)
+    # model = build_model_kfold(base_model, IMG_SHAPE, data_augmentation)
+    # history = model.fit(partial_train_data, partial_train_targets,
+    #                 validation_data=(val_data, val_targets),
+    #                 epochs=num_epochs, batch_size=BATCH_SIZE, verbose=0)
+    # accuracy_history = history.history["val_accuracy"]
+    # all_mae_histories.append(accuracy_history)
         # model compile
-
-        # model fit
-
 
